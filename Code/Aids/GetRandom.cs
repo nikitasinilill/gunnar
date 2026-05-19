8cf9c8a0fbc98f53f906cf3be5f3bb9c987d3e97
nikitasinilill <nikisi@taltech.ee>
2026-05-20 00:19:52 +0300

    Add extended random value helpers

--- Code/Aids/GetRandom.cs
+++ Code/Aids/GetRandom.cs
@@ -1,4 +1,5 @@
-﻿
+﻿using System.Reflection;
+
 namespace Abc.Aids;
 
 public static class GetRandom {
@@ -18,7 +19,84 @@ public static class GetRandom {
         if (min > max) (min, max) = (max, min);
         return min + r.NextDouble() * (max - min);
     }
-    // iseseisvalt teha ja testida
-    //    Int8, Int16, UInt8, UInt16, UInt32, UInt64, Decimal, Float
-    //    string, char, bool, DateTime, TimeSpan, Guid
+    public static sbyte Int8(sbyte min = sbyte.MinValue, sbyte max = sbyte.MaxValue)
+        => (sbyte) Int32(min, max);
+    public static short Int16(short min = short.MinValue, short max = short.MaxValue)
+        => (short) Int32(min, max);
+    public static byte UInt8(byte min = byte.MinValue, byte max = byte.MaxValue)
+        => (byte) Int32(min, max);
+    public static ushort UInt16(ushort min = ushort.MinValue, ushort max = ushort.MaxValue)
+        => (ushort) Int32(min, max);
+    public static uint UInt32(uint min = uint.MinValue, uint max = uint.MaxValue)
+        => (uint) Int64(min, max);
+    public static ulong UInt64(ulong min = ulong.MinValue, ulong max = ulong.MaxValue) {
+        var minLong = (long) min - long.MaxValue;
+        var maxLong = (long) max - long.MaxValue;
+        return (ulong) (Int64(minLong, maxLong) + long.MaxValue);
+    }
+    public static float Float(float min = float.MinValue, float max = float.MaxValue)
+        => (float) Double(min, max);
+    public static decimal Decimal(decimal min = decimal.MinValue, decimal max = decimal.MaxValue)
+        => (decimal) Double((double) min, (double) max);
+    public static Guid Guid() {
+        Span<byte> buffer = stackalloc byte[16];
+        r.NextBytes(buffer);
+        return new Guid(buffer);
+    }
+    public static TimeSpan TimeSpan(TimeSpan? min = null, TimeSpan? max = null) {
+        var minTicks = min?.Ticks ?? System.TimeSpan.MinValue.Ticks;
+        var maxTicks = max?.Ticks ?? System.TimeSpan.MaxValue.Ticks;
+        var ticks = Int64(minTicks, maxTicks);
+        return new TimeSpan(ticks);
+    }
+    public static DateTime DateTime(DateTime? min = null, DateTime? max = null) {
+        var minTicks = min?.Ticks ?? System.DateTime.MinValue.Ticks;
+        var maxTicks = max?.Ticks ?? System.DateTime.MaxValue.Ticks;
+        var ticks = Int64(minTicks, maxTicks);
+        return new DateTime(ticks);
+    }
+    public static bool Bool() => r.Next(2) == 0;
+    public static char Char(char min = char.MinValue, char max = char.MaxValue) => (char) UInt16(min, max);
+    public static string String(byte minLength = byte.MinValue, byte maxLength = (byte) sbyte.MaxValue) {
+        var len = UInt8(minLength, maxLength);
+        var s = new char[len];
+        for (var i = 0; i < len; i++) s[i] = Char('a', 'z');
+        return new string(s);
+    }
+    public static object Object(Type t) {
+        var x = Nullable.GetUnderlyingType(t);
+        if (x is not null) t = x;
+        var o = Activator.CreateInstance(t);
+        foreach (var p in t.GetProperties()) {
+            if (!p.CanWrite) continue;
+            if (p.PropertyType.IsArray) continue;
+            var v = isClass(p) ? Object(p.PropertyType): Value(p.PropertyType);
+            p.SetValue(o, v);
+        }
+        return o;
+    }
+    private static bool isClass(PropertyInfo p)
+        => p.PropertyType.IsClass && p.PropertyType != typeof(string);
+    private static object Value(Type t) {
+        var x = Nullable.GetUnderlyingType(t);
+        if (x is not null) t = x;
+        if (t == typeof(string)) return String();
+        if (t == typeof(char)) return Char();
+        if (t == typeof(bool)) return Bool();
+        if (t == typeof(DateTime)) return DateTime();
+        if (t == typeof(decimal)) return Decimal();
+        if (t == typeof(double)) return Double();
+        if (t == typeof(float)) return Float();
+        if (t == typeof(byte)) return UInt8();
+        if (t == typeof(ushort)) return UInt16();
+        if (t == typeof(uint)) return UInt32();
+        if (t == typeof(ulong)) return UInt64();
+        if (t == typeof(sbyte)) return Int8();
+        if (t == typeof(short)) return Int16();
+        if (t == typeof(int)) return Int32();
+        if (t == typeof(long)) return Int64();
+        if (t == typeof(TimeSpan)) return TimeSpan();
+        if (t == typeof(Guid)) return Guid();
+        throw new NotSupportedException($"Type {t} is not supported.");
+    }
 }
--- Tests/Aids/GetRandomTests.cs
+++ Tests/Aids/GetRandomTests.cs
@@ -1,14 +1,83 @@
 ﻿using Abc.Aids;
+using Abc.Data.Common;
 
 namespace Abc.Tests.Aids;
 
-[TestClass] public sealed class GetRandomTests {
+[TestClass]
+public sealed class GetRandomTests {
     private const sbyte min = sbyte.MinValue;
     private const sbyte max = sbyte.MaxValue;
-    [TestMethod] public void Int32Test() 
+    [TestMethod]
+    public void Int8Test()
+        => Assert.AreNotEqual(GetRandom.Int8(min, max), GetRandom.Int8(min, max));
+    [TestMethod]
+    public void Int16Test()
+        => Assert.AreNotEqual(GetRandom.Int16(min, max), GetRandom.Int16(min, max));
+    [TestMethod]
+    public void Int32Test()
         => Assert.AreNotEqual(GetRandom.Int32(min, max), GetRandom.Int32(min, max));
-    [TestMethod] public void Int64Test()
+    [TestMethod]
+    public void Int64Test()
         => Assert.AreNotEqual(GetRandom.Int64(min, max), GetRandom.Int64(min, max));
-    [TestMethod] public void DoubleTest()
+    [TestMethod]
+    public void UInt8Test()
+        => Assert.AreNotEqual(GetRandom.UInt8(0, (byte)max), GetRandom.UInt8(0, (byte)max));
+    [TestMethod]
+    public void UInt16Test()
+        => Assert.AreNotEqual(GetRandom.UInt16(0, (ushort)max), GetRandom.UInt16(0, (ushort)max));
+    [TestMethod]
+    public void UInt32Test()
+        => Assert.AreNotEqual(GetRandom.UInt32(0, (uint)max), GetRandom.UInt32(0, (uint)max));
+    [TestMethod]
+    public void UInt64Test()
+        => Assert.AreNotEqual(GetRandom.UInt64(0, (ulong)max), GetRandom.UInt64(0, (ulong)max));
+    [TestMethod]
+    public void DoubleTest()
         => Assert.AreNotEqual(GetRandom.Double(min, max), GetRandom.Double(min, max));
+    [TestMethod]
+    public void FloatTest()
+        => Assert.AreNotEqual(GetRandom.Float(min, max), GetRandom.Float(min, max));
+    [TestMethod]
+    public void DecimalTest()
+        => Assert.AreNotEqual(GetRandom.Decimal(min, max), GetRandom.Decimal(min, max));
+    [TestMethod]
+    public void StringTest()
+        => Assert.AreNotEqual(GetRandom.String(0, (byte)max), GetRandom.String(0, (byte)max));
+    [TestMethod]
+    public void CharTest()
+        => Assert.AreNotEqual(GetRandom.Char((char)0, (char)max), GetRandom.Char((char)0, (char)max));
+    [TestMethod]
+    public void BoolTest() {
+        var x = GetRandom.Bool();
+        bool y = GetRandom.Bool();
+        for (var i = 0; i < 10; i++) {
+            if (x != y) break;
+            y = GetRandom.Bool();
+        }
+        Assert.AreNotEqual(x, y);
+    }
+    [TestMethod]
+    public void DateTimeTest() {
+        var minDate = DateTime.Now.AddYears(-100);
+        var maxDate = DateTime.Now.AddYears(100);
+        Assert.AreNotEqual(GetRandom.DateTime(minDate, maxDate), GetRandom.DateTime(minDate, maxDate));
+    }
+    [TestMethod]
+    public void TimeSpanTest() {
+        var minSpan = TimeSpan.FromTicks(DateTime.Now.AddYears(-100).Ticks);
+        var maxSpan = TimeSpan.FromTicks(DateTime.Now.AddYears(100).Ticks);
+        Assert.AreNotEqual(GetRandom.TimeSpan(minSpan, maxSpan), GetRandom.TimeSpan(minSpan, maxSpan));
+    }
+    [TestMethod]
+    public void GuidTest()
+        => Assert.AreNotEqual(GetRandom.Guid(), GetRandom.Guid());
+    private class testClass: NamedEntity { }
+    [TestMethod] public void ObjectTest() {
+        var o1 = GetRandom.Object(typeof(testClass));
+        var o2 = GetRandom.Object(typeof(testClass));
+        foreach (var p in typeof(testClass).GetProperties()) {
+            if (p.PropertyType.IsArray) continue;
+            Assert.AreNotEqual(p.GetValue(o1), p.GetValue(o2));
+        }
+    }
 }
