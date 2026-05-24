19aa16d6e5528d94b659f488cffb1de45e0e64d0
nikitasinilill <nikisi@taltech.ee>
2026-05-24 21:44:09 +0300

    Make optional data relationship ids nullable

--- Code/Data/CountryCurrency.cs
+++ Code/Data/CountryCurrency.cs
@@ -3,7 +3,7 @@ using Abc.Data.Common;
 namespace Abc.Data;
 
 public class CountryCurrency: DetailedEntity {
-    public Guid CountryId { get; set; }
-    public Guid CurrencyId { get; set; }
+    public Guid? CountryId { get; set; }
+    public Guid? CurrencyId { get; set; }
     public Currency Currency { get; set; }
 }
--- Code/Data/Money.cs
+++ Code/Data/Money.cs
@@ -4,6 +4,6 @@ namespace Abc.Data;
 
 public class Money: BaseEntity {
     public decimal Amount { get; set; }
-    public Guid CurrencyId { get; set; }
+    public Guid? CurrencyId { get; set; }
     public Currency Currency { get; set; }
 }
